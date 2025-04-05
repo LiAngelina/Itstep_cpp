e986df086652a3944352619bc081158b61ba9f5f
LiAngelina <liangelina016@gmail.com>
2025-04-06 01:14:30 +0500

    My homework1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 90;
+    int b = 60;
+    int c = 50;
+
+    if (a > 120 && b > 120 && c > 120 && a < 700  && b < 700&& c < 700){
+         cout <<"OK";
+}
+    else {
+        cout << "error";
+    }
+}
